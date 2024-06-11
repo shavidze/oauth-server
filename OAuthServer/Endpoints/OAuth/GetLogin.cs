2b8c710aee024bc13433ab30f34780c621be710b
Saba Shavidze <sabashavidze@gmail.com>
2024-06-11 10:46:46 +0400

    feat: add getLogin / Login enpoints

--- OAuthServer/Endpoints/OAuth/GetLogin.cs
+++ OAuthServer/Endpoints/OAuth/GetLogin.cs
@@ -7,17 +7,18 @@ public static class GetLogin
     public static async Task Hanlder(string returnUrl, HttpResponse response)
     {
         response.Headers.ContentType = new string[] {  "text/html" };
-        await response.WriteAsync($"""
-                                   <html>
-                                     <head>
-                                        <title>Login</title>
-                                     </head>
-                                     <body>
-                                        <form action = "/login?returnUrl={HttpUtility.UrlEncode(returnUrl)}" method = "post">
-                                            <input value = "Submit" type="submit"/>
-                                        </form>
-                                     </body>
-                                   </html>
-                                   """);
+        await response.WriteAsync(
+            $"""
+             <html>
+               <head>
+                  <title>Login</title>
+               </head>
+               <body>
+                  <form action = "/login?returnUrl={HttpUtility.UrlEncode(returnUrl)}" method = "post">
+                      <input value = "Submit" type="submit"/>
+                  </form>
+               </body>
+             </html>
+             """);
     }
 }
